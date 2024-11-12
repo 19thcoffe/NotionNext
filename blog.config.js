13588a097bb575ecf7dca62c5e473a37dc661ceb
19thcoffe <19thcoffee@gmail.com>
2024-11-12 11:35:34 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -312,7 +312,7 @@ const BLOG = {
     {
       name: '曾经是情侣',
       artist: '',
-      url: 'ftp://guhui:840213@45.142.157.161/320.mp3',
+      url: 'http://music.163.com/song/media/outer/url?id=1493994302.mp3',
       cover:
         ''
     },
